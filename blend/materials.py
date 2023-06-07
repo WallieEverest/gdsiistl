9c64dc6f80f0b3419b3c4774e390cb01b29246ae
Wallie Everest <wallie.everest@gmail.com>
2023-06-07 10:52:40 -0400

    Blender script WIP

--- blend/materials.py
+++ blend/materials.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # Blender materials for Sky130 PDK layers
+# WORK IN PROGRESS
 import bpy
 
 bpy.ops.object.delete(use_global=False, confirm=False)
@@ -8,6 +9,13 @@ bpy.context.space_data.recent_folders_active = 1
 bpy.context.space_data.params.filename = "sample.blend"
 bpy.context.space_data.show_word_wrap = True
 bpy.context.space_data.show_word_wrap = False
+bpy.context.object.data.clip_end = 10000
+bpy.context.space_data.context = 'WORLD'
+bpy.context.space_data.context = 'RENDER'
+bpy.context.scene.render.engine = 'CYCLES'
+bpy.context.space_data.context = 'WORLD'
+bpy.data.worlds["World"].node_tree.nodes["Sky Texture"].sun_disc = False
+bpy.ops.outliner.item_activate(deselect_all=True)
 bpy.context.space_data.shading.type = 'SOLID'
 bpy.context.space_data.context = 'MATERIAL'
 bpy.ops.material.new()
